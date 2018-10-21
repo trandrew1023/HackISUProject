74271c65258041ea9e61c6801f0242191be8f3ad
Ethan Young <eyoung20@iastate.edu>
2018-10-20 19:09:06 -0500

    finished pulling from GPIO to random

--- Project/RandomChar/GPIO.py
+++ Project/RandomChar/GPIO.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 from RandomChar.CharArray import AddChar
-import random
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,9 +13,27 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 i = 0
 j = 0
-temp = AddChar()
+rand = AddChar()
 while len(temp.get16()) <= 16:
-    temp.addchar(random.randint(0, 255))
+    bit0 = GPIO.input(26) * 1
+    bit1 = GPIO.input(19) * 2
+    bit2 = GPIO.input(13) * 4
+    bit3 = GPIO.input(6) * 8
+    bit4 = GPIO.input(17) * 16
+    bit5 = GPIO.input(18) * 32
+    bit6 = GPIO.input(22) * 64
+    bit7 = GPIO.input(23) * 128
+    byte = bit0 + bit1 + bit2 + bit3 + bit4 + bit5 + bit6 + bit7
+    rand.addchar(byte)
 
 while len(temp.get32()) <= 32:
-    temp.addchar(random.randint(0, 255))
+    bit0 = GPIO.input(26) * 1
+    bit1 = GPIO.input(19) * 2
+    bit2 = GPIO.input(13) * 4
+    bit3 = GPIO.input(6) * 8
+    bit4 = GPIO.input(17) * 16
+    bit5 = GPIO.input(18) * 32
+    bit6 = GPIO.input(22) * 64
+    bit7 = GPIO.input(23) * 128
+    byte = bit0 + bit1 + bit2 + bit3 + bit4 + bit5 + bit6 + bit7
+    rand.addchar(byte)
